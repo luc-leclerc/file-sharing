addc9609502ac8f8b6f12168550f00feb6a4107b
luc-leclerc <leclerc.luc@gmail.com>
2022-06-24 11:18:56 -0400

    wip

--- src/main/java/noorg/filesharing/Main.java
+++ src/main/java/noorg/filesharing/Main.java
@@ -1,9 +1,12 @@
 package noorg.filesharing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/")})
 public class Main {
 
     public static void main(String[] args) {
